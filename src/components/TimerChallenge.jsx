d3324db594b5cf77fefb71c31ab5295272d18048
arianoorqorbani <aria.noorghorbani@gmail.com>
2023-12-19 12:00:09 +0300

    add state to timer challenge

--- src/components/TimerChallenge.jsx
+++ src/components/TimerChallenge.jsx
@@ -1,14 +1,30 @@
+import { useState } from "react";
+
 export default function TimerChallenge({ title, targetTime }) {
+  const [isLost, setIsLost] = useState(false);
+  const [isStarted, setIsStarted] = useState(false);
+
+  function handleStart() {
+    setTimeout(() => {
+      setIsLost(true);
+    }, targetTime * 1000);
+    setIsStarted(true);
+  }
   return (
     <section className="challenge">
       <h2>{title}</h2>
+      {isLost && "You are lost"}
       <p className="challenge-time">
         {targetTime} second{targetTime > 1 ? "s" : ""}
       </p>
       <p>
-        <button>Start Challenge</button>
+        <button onClick={handleStart}>
+          {isStarted ? "Stop" : "Start"} Challenge
+        </button>
       </p>
-      <p className="">Time is running.../ Time is inactive</p>
+      <p className={isStarted ? "active" : undefined}>
+        {isStarted ? "Time is running..." : "Time is inactive"}
+      </p>
     </section>
   );
 }
