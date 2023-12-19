3e2dcf9f1b9802e6ccaa5c8b0f4c49295d5f3f78
arianoorqorbani <aria.noorghorbani@gmail.com>
2023-12-19 10:16:25 +0300

    initial commit

---
+++ src/components/Player.jsx
@@ -0,0 +1,24 @@
+import { useState } from "react";
+
+export default function Player() {
+  const [changePlayer, setChangePlayer] = useState("");
+  const [submit, setSubmit] = useState(false);
+
+  function handleChange(event) {
+    setChangePlayer(event.target.value);
+  }
+
+  function handleSubmit() {
+    setSubmit(true);
+  }
+
+  return (
+    <section id="player">
+      <h2>Welcome {submit ? changePlayer : "unknown entity"} </h2>
+      <p>
+        <input type="text" onChange={handleChange} />
+        <button onClick={handleSubmit}>Set Name</button>
+      </p>
+    </section>
+  );
+}
